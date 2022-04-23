784315ca648dd879e339213a99826cd006d3df3d
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 01:14:19 +0300

    Add changes to class init method

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 class User:
     #class that is for new instances for users
 
-        def __init__(self,first_name,last_name,phone_number,email):
+        def __init__(self,first_name,last_name,phone_number,email,password):
 
         
