c771fdd824dee2b9ecbaeff1368aeebf75ca42c3
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 01:21:42 +0300

    Add class variables for our function

--- user.py
+++ user.py
@@ -4,3 +4,7 @@ class User:
         def __init__(self,first_name,last_name,phone_number,email,password):
 
         
+            self.first_name = first_name
+            self.last_name = last_name
+            self.phone_number = number
+            self.email = email
