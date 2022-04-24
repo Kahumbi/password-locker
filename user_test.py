03cb1c55b21fa3e803268a68119d477ea34d839f
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 11:29:30 +0300

    Addfirst succesfull ran test

--- user_test.py
+++ user_test.py
@@ -5,5 +5,29 @@ class TestUser(unittest.TestCase):
 
     '''
     A sub class that inherits from the parent class.
+    '''
+
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("James","Muriuki","0712345678","james@ms.com","zxcvb") # the user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"James")
+        self.assertEqual(self.new_user.last_name,"Muriuki")
+        self.assertEqual(self.new_user.phone_number,"0712345678")
+        self.assertEqual(self.new_user.email,"james@ms.com")
+        self.assertEqual(self.new_user.password,"zxcvb")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
   
