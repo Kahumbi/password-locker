c28d5e5cf20682b4f00f3276615197ff5f84c309
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 10:03:39 +0300

    Adda sub class that inherits from the parent class

---
+++ user_test.py
@@ -0,0 +1,9 @@
+import unittest # Importing the unittest module
+from user import User # Importing the contact class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    A sub class that inherits from the parent class.
+
+  
