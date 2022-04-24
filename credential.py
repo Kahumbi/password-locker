9e5b1a45574546e85b3fa557f87f258f5b54de62
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 18:42:18 +0300

    Add a credentials class

---
+++ credential.py
@@ -0,0 +1,10 @@
+class Credential:
+
+    credential_list = [] 
+
+    def __init__(self,accountname,username,password):
+
+
+        self.accountname = accountname
+        self.username = username
+        self.password = password 
