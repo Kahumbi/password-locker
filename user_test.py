f91a67dfdc3917c2e478e6e1a637ef993a227e63
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-24 13:58:59 +0300

    Add a tear down method that runs after every test

--- user_test.py
+++ user_test.py
@@ -30,12 +30,33 @@ class TestUser(unittest.TestCase):
     def test_save_user(self):
         '''
         test_save_user test case to test if the user object is saved into
-         the user list
+        the user list
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
 
-if __name__ ==  '__main__':
+
+
+   
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+# other test cases here
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_userto check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712345678","test@user.com","zxcvb") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+    
+          
+if __name__ == '__main__':
     unittest.main()
 
 
